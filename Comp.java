8bcb4dc1c645cac5063388ab2b338b3e481a3b45
Ruba100 <127715815+Ruba100@users.noreply.github.com>
2024-05-10 16:14:47 +0300

    Add files via upload

---
+++ Comp.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package comp;
+
+/**
+ *
+ * @author Ruba
+ */
+import java.util.Scanner;
+public class Comp {
+//     public static void main(String[] args) {
+//      Scanner scanner = new Scanner(System.in);
+//        System.out.println("enter a String input");
+//        String input = scanner.nextLine();
+//        String inputL = input.replaceAll("\\s+", "");
+//        System.out.println(inputL);
+//         System.out.println();
+//         System.out.println(input);
+//         
+
+    
+}
+    
+
