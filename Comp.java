1ab7e3b3f2e49c5c1208b9bf8760f211a4e6e6ef
Ruba100 <127715815+Ruba100@users.noreply.github.com>
2024-05-10 16:15:53 +0300

    Update Comp.java

--- Comp.java
+++ Comp.java
@@ -1,26 +1,38 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
-package comp;
-
-/**
- *
- * @author Ruba
- */
 import java.util.Scanner;
-public class Comp {
-//     public static void main(String[] args) {
-//      Scanner scanner = new Scanner(System.in);
-//        System.out.println("enter a String input");
-//        String input = scanner.nextLine();
-//        String inputL = input.replaceAll("\\s+", "");
-//        System.out.println(inputL);
-//         System.out.println();
-//         System.out.println(input);
-//         
 
-    
+public class complierProject {
+
+    public static void main(String[] args) {
+
+
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter a String input:");
+        String inputu = input.nextLine();
+        // يستبدل كل مسافه ب "" يعني يخليها بدون مسافات
+        String inputL = inputu.replaceAll("\\s+", "");
+        System.out.println(inputL);
+        System.out.println();
+
+ // اراي تفصل لجمله حسب المسافه
+        String[] words = inputu.split("\\s+");
+
+        for (int i = 0; i < words.length; i++) {
+            String x = maenTheword(words[i]);
+            System.out.println(words[i] + " ----> " + x);
+        }
+    }
+
+     public static String maenTheword(String word) {
+        if (word.equals("int") || word.equals("double") || word.equals("float")) {
+            return "data type";
+        } else if(word.matches("[a-zA-Z0-9]+")){
+            return"identifier";
+        }else if(word.equals(";")){
+            return "SEMI_COLON";
+        }else if(word.equals("=")){
+            return" ASSIGN_OP";
+        }
+        return"";
+    }
+
 }
-    
-
